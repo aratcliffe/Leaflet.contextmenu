745e33a61b81b519cec13a22b1be16426d4f7434
Adam Ratcliffe <adam.ratcliffe@gmail.com>
2016-12-20 14:58:24 +1300

    Add retinaIcon and retinaIconCls menu item options. Tabs to spaces

--- src/Mixin.ContextMenu.js
+++ src/Mixin.ContextMenu.js
@@ -1,108 +1,108 @@
 L.Mixin.ContextMenu = {
-	bindContextMenu: function (options) {
-		L.setOptions(this, options);
-		this._initContextMenu();
+    bindContextMenu: function (options) {
+        L.setOptions(this, options);
+        this._initContextMenu();
 
-		return this;
-	},
+        return this;
+    },
 
-	unbindContextMenu: function (){
-		this.off('contextmenu', this._showContextMenu, this);
+    unbindContextMenu: function (){
+        this.off('contextmenu', this._showContextMenu, this);
 
-		return this;
-	},
+        return this;
+    },
 
-	addContextMenuItem: function (item) {
-			this.options.contextmenuItems.push(item);
-	},
+    addContextMenuItem: function (item) {
+            this.options.contextmenuItems.push(item);
+    },
 
-	removeContextMenuItemWithIndex: function (index) {
-		var items = [];
-		for (var i = 0; i < this.options.contextmenuItems.length; i++) {
-			if (this.options.contextmenuItems[i].index == index){
-				items.push(i);
-			}
-		}
-		var elem = items.pop();
-		while (elem !== undefined) {
-			this.options.contextmenuItems.splice(elem,1);
-			elem = items.pop();
-		}
-	},
+    removeContextMenuItemWithIndex: function (index) {
+        var items = [];
+        for (var i = 0; i < this.options.contextmenuItems.length; i++) {
+            if (this.options.contextmenuItems[i].index == index){
+                items.push(i);
+            }
+        }
+        var elem = items.pop();
+        while (elem !== undefined) {
+            this.options.contextmenuItems.splice(elem,1);
+            elem = items.pop();
+        }
+    },
 
-	replaceContextMenuItem: function (item) {
-		this.removeContextMenuItemWithIndex(item.index);
-		this.addContextMenuItem(item);
-	},
+    replaceContextMenuItem: function (item) {
+        this.removeContextMenuItemWithIndex(item.index);
+        this.addContextMenuItem(item);
+    },
 
-	_initContextMenu: function () {
-		this._items = [];
+    _initContextMenu: function () {
+        this._items = [];
 
-		this.on('contextmenu', this._showContextMenu, this);
-	},
+        this.on('contextmenu', this._showContextMenu, this);
+    },
 
-	_showContextMenu: function (e) {
-		var itemOptions,
-			data, pt, i, l;
+    _showContextMenu: function (e) {
+        var itemOptions,
+            data, pt, i, l;
 
-		if (this._map.contextmenu) {
-			data = L.extend({relatedTarget: this}, e);
+        if (this._map.contextmenu) {
+            data = L.extend({relatedTarget: this}, e);
 
-			pt = this._map.mouseEventToContainerPoint(e.originalEvent);
+            pt = this._map.mouseEventToContainerPoint(e.originalEvent);
 
-			if (!this.options.contextmenuInheritItems) {
-				this._map.contextmenu.hideAllItems();
-			}
+            if (!this.options.contextmenuInheritItems) {
+                this._map.contextmenu.hideAllItems();
+            }
 
-			for (i = 0, l = this.options.contextmenuItems.length; i < l; i++) {
-				itemOptions = this.options.contextmenuItems[i];
-				this._items.push(this._map.contextmenu.insertItem(itemOptions, itemOptions.index));
-			}
+            for (i = 0, l = this.options.contextmenuItems.length; i < l; i++) {
+                itemOptions = this.options.contextmenuItems[i];
+                this._items.push(this._map.contextmenu.insertItem(itemOptions, itemOptions.index));
+            }
 
-			this._map.once('contextmenu.hide', this._hideContextMenu, this);
+            this._map.once('contextmenu.hide', this._hideContextMenu, this);
 
-			this._map.contextmenu.showAt(pt, data);
-		}
-	},
+            this._map.contextmenu.showAt(pt, data);
+        }
+    },
 
-	_hideContextMenu: function () {
-		var i, l;
+    _hideContextMenu: function () {
+        var i, l;
 
-		for (i = 0, l = this._items.length; i < l; i++) {
-			this._map.contextmenu.removeItem(this._items[i]);
-		}
-		this._items.length = 0;
+        for (i = 0, l = this._items.length; i < l; i++) {
+            this._map.contextmenu.removeItem(this._items[i]);
+        }
+        this._items.length = 0;
 
-		if (!this.options.contextmenuInheritItems) {
-			this._map.contextmenu.showAllItems();
-		}
-	}
+        if (!this.options.contextmenuInheritItems) {
+            this._map.contextmenu.showAllItems();
+        }
+    }
 };
 
 var classes = [L.Marker, L.Path],
-	defaultOptions = {
-		contextmenu: false,
-		contextmenuItems: [],
-		contextmenuInheritItems: true
-	},
-	cls, i, l;
+    defaultOptions = {
+        contextmenu: false,
+        contextmenuItems: [],
+        contextmenuInheritItems: true
+    },
+    cls, i, l;
 
 for (i = 0, l = classes.length; i < l; i++) {
-	cls = classes[i];
+    cls = classes[i];
 
-	// L.Class should probably provide an empty options hash, as it does not test
-	// for it here and add if needed
-	if (!cls.prototype.options) {
-		cls.prototype.options = defaultOptions;
-	} else {
-		cls.mergeOptions(defaultOptions);
-	}
+    // L.Class should probably provide an empty options hash, as it does not test
+    // for it here and add if needed
+    if (!cls.prototype.options) {
+        cls.prototype.options = defaultOptions;
+    } else {
+        cls.mergeOptions(defaultOptions);
+    }
 
-	cls.addInitHook(function () {
-		if (this.options.contextmenu) {
-			this._initContextMenu();
-		}
-	});
+    cls.addInitHook(function () {
+        if (this.options.contextmenu) {
+            this._initContextMenu();
+        }
+    });
 
-	cls.include(L.Mixin.ContextMenu);
+    cls.include(L.Mixin.ContextMenu);
 }
