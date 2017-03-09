e1c1c5fa73c7076e292bfff7f5bee45612a0007f
Adam Ratcliffe <adam.ratcliffe@gmail.com>
2017-03-10 09:29:28 +1300

    Return menu item(s) when removing from menu; fix typo in contextmenu.select event

--- src/Map.ContextMenu.js
+++ src/Map.ContextMenu.js
@@ -119,16 +119,22 @@ L.Map.ContextMenu = L.Handler.extend({
                 contextmenu: this,
                 el: item
             });
+
+            return item;
         }
+
+        return null;
     },
 
     removeAllItems: function () {
-        var item;
+        var items = this._container.children,
+            item;
 
-        while (this._container.children.length) {
-            item = this._container.children[0];
+        while (items.length) {
+            item = items[0];
             this._removeItem(L.Util.stamp(item));
         }
+        return items;
     },
 
     hideAllItems: function () {
@@ -297,7 +303,7 @@ L.Map.ContextMenu = L.Handler.extend({
                 func.call(context || map, me._showLocation);
             }
 
-            me._map.fire('contextmenu:select', {
+            me._map.fire('contextmenu.select', {
                 contextmenu: me,
                 el: el
             });
